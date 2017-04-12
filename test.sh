2629335542c68d50073ae787b77e9c3fb6e88ef6
Mark Oliver <aerialcarrot@users.noreply.github.com>
2017-04-12 11:47:10 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ echo --- Integration Tests ---
 
 ### Variables ###
 
-hostname="alpine-nginx"
+hostname="alpine-nginx-test"
 application="Test Alpine Nginx Container $(date +$s)" #randomized
 
 echo Hostname: ${hostname}
